93358f0afd2562d880b60f6997b215641bf6c7b6
Ricardo Escobar <jorge.ricardo.escobar@gmail.com>
2020-01-01 19:02:30 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+// Funcion calcula el valor de un dado
+
+function tiraDado() {
+    // Elegir un numero al azar entre 0 y 5
+
+    var valorDado = Math.random() * 6; // 0.215165454
+    return Math.floor(valorDado); // 0.0 al 5.0
+}
+
+// Arreglo con las URL de las imagenes
+
+var imagenes = [
+    "images/dice1.png", // 0
+    "images/dice2.png", // 1
+    "images/dice3.png", // 2
+    "images/dice4.png", // 3
+    "images/dice5.png", // 4
+    "images/dice6.png"  // 5
+];
+
+// 1 cambiar imagen al dado 1(valorDado1 + valorDado2) + 2; //
+document.querySelector(".img1").src = imagenes[valorDado1];
+document.querySelector(".img2").src = imagenes[valorDado2];
+
+// Imprimir ganador
+
+if (valorDado1 > valorDado2) {
+    document.querySelector("h1").textContent = "Player 1 WINS!";
+} else if (valorDado1 < valorDado2) {
+    document.querySelector("h1").textContent = "Player 2 WINS!";
+} else {
+    document.querySelector("h1").textContent = "It's a TIE!";
+}
